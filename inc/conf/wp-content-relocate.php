030ed71d06c88cc0cdc788a915ce40c8485152db
rezat42 <alireza1964@live.com>
2015-09-21 16:04:45 -0500

    First commit

---
+++ inc/conf/wp-content-relocate.php
@@ -0,0 +1,9 @@
+<?php
+// Relocating the wp-content folder
+
+   
+ define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/core/asset');
+  define('WP_CONTENT_URL', $_SERVER['SERVER_NAME'] .'/core/asset');
+   define('WP_PLUGIN_DIR', $_SERVER['DOCUMENT_ROOT'] . 'core/asset/plugins');
+    define('WP_PLUGIN_URL',  $_SERVER['SERVER_NAME'] .'core/content/plugins');
+     define('PLUGINDIR', $_SERVER['DOCUMENT_ROOT'] . 'core/asset/plugins'); 
---
+++ inc/env/wp-config.env.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Setup environments
+ * 
+ * Set environment based on the current server hostname, this is stored
+ * in the $hostname variable
+ * 
+ * You can define the current environment via: 
+ *     define('WP_ENV', 'production');
+ * 
+ * @package    Studio 24 WordPress Multi-Environment Config
+ * @version    1.0
+ * @author     Studio 24 Ltd  <info@studio24.net>
+ */
+
+
+/*
+ * Set environment based on hostname
+ *
+ * If you just use localhost for your local test environment then in place of:
+ *   case 'domain.dev':
+ *
+ * Just use:
+ *   case 'localhost':
+ *
+ */
+switch ($hostname) {
+    case 'holo.dev':
+        define('WP_ENV', 'dev');
+        break;
+    
+    case 'dev.stage':
+        define('WP_ENV', 'stage');
+        break;
+
+    case 'dev.prod':
+    default: 
+        define('WP_ENV', 'prod');
+}
+
